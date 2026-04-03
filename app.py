95bea9907978f3b94fe06da26063ffcb93c8210d
Maurício Augusto <mrc_augusto@hotmail.com>
2026-04-03 16:04:46 -0300

    Implement payment creation and confirmation endpoints with validation

--- app.py
+++ app.py
@@ -1,23 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
+from db_models.payment import Payment
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'SECRET_KET_WEBSOCKET'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "SECRET_KET_WEBSOCKET"
 
 db.init_app(app)
 
-@app.route('/payments/pix', methods=['POST'])
+
+@app.route("/payments/pix", methods=["POST"])
 def create_payment_pix():
-  return jsonify({'message': 'Payment created sucessfully'})
+    data = request.get_json()
 
-@app.route('/payments/pix/confirmation', methods=['POST'])
+    if "value" not in data:
+        return jsonify({"message": "Invalid value"}), 400
+
+    expiration_date = datetime.now() + timedelta(minutes=30)
+    new_payment = Payment(value=data["value"], expiration_date=expiration_date)
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify(
+        {"message": "Payment created sucessfully", "payment": new_payment.to_dict()}
+    )
+
+
+@app.route("/payments/pix/confirmation", methods=["POST"])
 def pix_confirmation():
-  return jsonify({'message': 'Payment confirmed sucessfully'})
+    return jsonify({"message": "Payment confirmed sucessfully"})
 
-@app.route('/payments/pix/<int:payment_id>', methods=['GET'])
+
+@app.route("/payments/pix/<int:payment_id>", methods=["GET"])
 def payment_pix_page(payment_id):
-  return 'pagamento pix'
+    return "pagamento pix"
 
-if __name__ == '__main__':
-  app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
