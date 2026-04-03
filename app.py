f14fd799382b3bd2cf553f7033cf4b43d78c44ab
Maurício Augusto <mrc_augusto@hotmail.com>
2026-04-03 12:57:55 -0300

    Initialize Flask application with payment routes and database setup

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from repository.database import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KET_WEBSOCKET'
+
+db.init_app(app)
+
+@app.route('/payments/pix', methods=['POST'])
+def create_payment_pix():
+  return jsonify({'message': 'Payment created sucessfully'})
+
+@app.route('/payments/pix/confirmation', methods=['POST'])
+def pix_confirmation():
+  return jsonify({'message': 'Payment confirmed sucessfully'})
+
+@app.route('/payments/pix/<int:payment_id>', methods=['GET'])
+def payment_pix_page(payment_id):
+  return 'pagamento pix'
+
+if __name__ == '__main__':
+  app.run(debug=True)
