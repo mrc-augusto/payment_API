5c6dd68a5671cfb3d8ade2eee0572d426129a40a
Maurício Augusto <mrc_augusto@hotmail.com>
2026-04-14 14:51:33 -0300

    Ignore generated QR code images

--- payments/pix.py
+++ payments/pix.py
@@ -1,4 +1,5 @@
 import uuid
+import os
 import qrcode
 
 
@@ -6,7 +7,7 @@ class Pix:
     def __init__(self):
         pass
 
-    def create_payment_pix(self):
+    def create_payment_pix(self, base_dir=""):
         # Gerar um ID único para o pagamento
         bank_payment_id = str(uuid.uuid4())
         # Gera código QR para o pagamento
@@ -14,7 +15,8 @@ class Pix:
         # QR Code generation
         img = qrcode.make(hash_payment)
         # Salvar a imagem do QR Code
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        os.makedirs(f"{base_dir}static/img", exist_ok=True)
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
         return {
             "bank_payment_id": bank_payment_id,
